0103df6da1f9f98f409063153abc1fdc77afa936
Rafael Borrego <rafaborrego@gmail.com>
2018-10-19 23:14:40 +0100

    Added a script to generate migration files, an example of configuration to connect to a local database and examples of migration files

---
+++ flyway/migrations/V1.0_0_201810182230__JIRA-123.sql
@@ -0,0 +1,7 @@
+create table EMPLOYEE 
+        (ID     mediumint not null auto_increment,
+         NAME   varchar(255), 
+         OFFICE varchar(255), 
+         MOBILE varchar(255),
+         primary key (ID));
+
---
+++ flyway/migrations/V1.0_0_201810182234__JIRA-234.sql
@@ -0,0 +1,4 @@
+insert into EMPLOYEE  (NAME, OFFICE, MOBILE)
+values
+  ('John', 'Central London office', '077070707'),
+  ('Paul', 'Canary Wharf office', '077080808');
